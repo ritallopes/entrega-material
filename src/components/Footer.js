17cf33961d1ac7585f33cd5d58d5596a263f21f2
Rita Lopes <41452534+ritallopes@users.noreply.github.com>
2021-09-13 09:17:16 -0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
+import {Row, Col, Container} from "react-bootstrap'
 export default function Footer(){
     return(
-        <div style={{position: "fixed", bottom:0, textAlign:"center"}}>ritallopes</div>
+        <Container style={{position: "fixed", bottom:0, textAlign:"center"}}><Row><Col className="text-center">ritallopes</Col></Row></Container>
     )
 }
